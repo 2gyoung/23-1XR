412148eb49a04e47ad604806e3974aa5db0f2d12
cea-rin <119026096+cea-rin@users.noreply.github.com>
2023-06-15 14:55:47 +0900

    one-touch

--- Assets/Scripts/FoodMoveController.cs
+++ Assets/Scripts/FoodMoveController.cs
@@ -8,7 +8,6 @@ using UnityEngine;
 
 public class FoodMoveController : MonoBehaviour
 {
-
     private const float CameraDistance = 7.5f;
     public float positionY = 0.4f;
     public GameObject[] prefab;
@@ -16,6 +15,7 @@ public class FoodMoveController : MonoBehaviour
     protected Camera mainCamera;
     protected GameObject HoldingObject;
     protected Vector3 InputPosition;
+    protected bool isHolding; // 추가: 음식을 들고 있는 상태 여부
 
     void Start()
     {
@@ -30,38 +30,42 @@ public class FoodMoveController : MonoBehaviour
 #endif
         InputPosition = TouchHelper.TouchPosition;
 
-        if (TouchHelper.Touch2)
-        {
-            Reset();
-            return;
-        }
-
         if (HoldingObject)
         {
-            if (TouchHelper.IsUp)
+            if (TouchHelper.IsUp && isHolding) // 수정: 음식을 들고 있는 상태에서 터치를 놓았을 때
             {
                 OnPut(InputPosition);
-                HoldingObject = null;
+                isHolding = false; // 추가: 음식을 들고 있는 상태 해제
+                HoldingObject = null; // 수정: 들고 있던 음식 객체 초기화
                 return;
             }
 
-            Move(InputPosition);
+            if (isHolding) // 추가: 음식을 들고 있는 상태일 때만 이동
+            {
+                Move(InputPosition);
+            }
             return;
         }
 
-        if (!TouchHelper.IsDown) return;
-        if (Physics.Raycast(mainCamera.ScreenPointToRay(InputPosition), out var hits, mainCamera.farClipPlane))
+        if (TouchHelper.IsDown)
         {
-            if (hits.transform.gameObject.tag.Equals("Food"))
-            {
-                HoldingObject = hits.transform.gameObject;
-                OnHold();
-            }
+            // 수정: 터치한 위치에 음식 생성
+            var spawnPos = mainCamera.ScreenToWorldPoint(new Vector3(InputPosition.x, InputPosition.y, CameraDistance));
+            var index = Random.Range(0, prefab.Length);
+            HoldingObject = Instantiate(prefab[index], spawnPos, Quaternion.identity);
+            OnHold();
+            isHolding = true; // 추가: 음식을 들고 있는 상태로 변경
         }
     }
 
     protected virtual void OnPut(Vector3 pos)
     {
+        /*        var rigidbody = HoldingObject.GetComponent<Rigidbody>();
+                rigidbody.useGravity = true;
+                var direction = mainCamera.transform.TransformDirection(Vector3.forward).normalized;
+                var delta = (pos.y - InputPosition.y) * 100f / Screen.height;
+                rigidbody.AddForce((direction + Vector3.up) * 2.5f * delta, ForceMode.Impulse); // 수정: 던지는 힘을 impulse로 변경
+                HoldingObject.transform.SetParent(null);*/
         HoldingObject.GetComponent<Rigidbody>().useGravity = true;
         HoldingObject.transform.SetParent(null);
     }
@@ -69,7 +73,7 @@ public class FoodMoveController : MonoBehaviour
     private void Move(Vector3 pos)
     {
         pos.z = mainCamera.nearClipPlane * CameraDistance;
-        HoldingObject.transform.position = Vector3.Lerp( HoldingObject.transform.position, 
+        HoldingObject.transform.position = Vector3.Lerp(HoldingObject.transform.position,
             mainCamera.ScreenToWorldPoint(pos),
             Time.deltaTime * 7f);
     }
@@ -81,12 +85,13 @@ public class FoodMoveController : MonoBehaviour
         HoldingObject.transform.SetParent(mainCamera.transform);
         HoldingObject.transform.rotation = Quaternion.identity;
         HoldingObject.transform.position =
-            mainCamera.ViewportToWorldPoint( new Vector3(0.5f, positionY, mainCamera.nearClipPlane * CameraDistance));
+            mainCamera.ViewportToWorldPoint(
+                new Vector3(0.5f, positionY, mainCamera.nearClipPlane * CameraDistance));
     }
 
     private void Reset()
     {
-        var pos = mainCamera.ViewportToWorldPoint(new Vector3( 0.5f, positionY, mainCamera.nearClipPlane * CameraDistance));
+        var pos = mainCamera.ViewportToWorldPoint(new Vector3(0.5f, positionY, mainCamera.nearClipPlane * CameraDistance));
         var index = Random.Range(0, prefab.Length);
         var obj = Instantiate(prefab[0], pos, Quaternion.identity, mainCamera.transform);
         var rigidbody = obj.GetComponent<Rigidbody>();
--- Assets/Scripts/ThrowFoodController.cs
+++ Assets/Scripts/ThrowFoodController.cs
@@ -2,33 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 /*
  먹이 던지기 기능 구현
  */
 
 public class ThrowFoodController : FoodMoveController
 {
-
     private Vector2 _inputPositionPivot;
+    public float throwForce = 100f;
+    private bool _isHolding = false;
+
+
 
     protected override void OnPut(Vector3 pos)
     {
-        var rigidbody = HoldingObject.GetComponent<Rigidbody>();
-        rigidbody.useGravity = true;
-        var direction = mainCamera.transform.TransformDirection(Vector3.forward).normalized;
-        var delta = (pos.y - _inputPositionPivot.y) * 100f / Screen.height;
-        rigidbody.AddForce((direction + Vector3.up) * 2.5f * delta);
-        HoldingObject.transform.SetParent(null);
-        _inputPositionPivot.y = pos.y;
-    }
-    
-    protected override void OnHold()
-    {
-        _inputPositionPivot = InputPosition;
+        if (_isHolding)
+        {
+            var rigidbody = HoldingObject.GetComponent<Rigidbody>();
+            rigidbody.useGravity = true;
+            var direction = mainCamera.transform.forward;
+            var delta = (pos.y - _inputPositionPivot.y) * 100f / Screen.height;
+            var throwForce = 2.5f * delta;
+            var throwVector = direction + mainCamera.transform.up + mainCamera.transform.forward * throwForce;
+            rigidbody.AddForce((direction + Vector3.up) * 4.5f * delta);
+            HoldingObject.transform.SetParent(null);
+            _isHolding = false;
+
+        }
     }
 
-    //먹이가 화면에서 나가면 삭제
+
+
+    protected override void OnHold()
+    {
+        if (!_isHolding && Input.touchCount == 1)
+        {
+            var touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                _inputPositionPivot = touch.position;
+                _isHolding = true;
+            }
+        }
+    }
+
+    // 먹이가 화면 밖으로 나가면 삭제
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
